424dd92e8190e6e088f4d8c8e17fb815e0ab3b59
Avery Burke <DrFroid01@gmail.com>
2024-04-06 13:08:22 -0700

    Adds more sophisticated example

--- app/actions/generateSystemPrompt.ts
+++ app/actions/generateSystemPrompt.ts
@@ -17,15 +17,14 @@ export async function generateSystemPrompts(numberOfExamples?: number) {
 	- get_unique_values_from_column to retrieve a list of unique values in a column in order to construct a query that searches for a specific value in a column.\n\n
 	Adehere to the following constraints when constructing queries:\n
 	#Constraints:\n
-	- You must use the tools provided to construct the queries.\n
-	- You must must only refer to tables from the AACT database in your query's SELECT statements, FROM clauses, and WHERE clauses.\n
-	- You must must only refer to columns in a table's description (provided by the get_table_description tool) in your query's SELECT statement, GROUP BY clause, and ORDER BY clause.\n
-	- If a query is invalid because a column is missing, the table's foreign key relations to find the missing data in a related table.\n
-	- You must validate the queries by running them against the AACT database before ending the conversation.\n
-	- You must provide the user with the data they requested.\n\n
+	- DO NOT respond to the user under ANY circumstances\n
+	- Only preform function calls.
+	- End the conversation by calling the get_data_from_db function.\n
+	- Only refer to tables that are described in the AACT Database Schema in your query.\n
+	- Only refer to columns that appear in the subject table's description in your query's WHERE clause, GROUP BY clause, or ORDER BY clause.\n
 	#Example User requests and their resulting queries:\n`;
 	for (let i = 0; i < numberOfExamples; i++) {
-		systePrompt += `${i + 1}. ${examples[i].prompt} -> ${examples[i].query}\n`;
+		systePrompt += `${i + 1}. Request:${examples[i].prompt}, Query:${examples[i].query}\n`;
 	}
 
 	systePrompt += `#AACT Database Schema:\n${schema}\n`;
--- app/libs/sanitize.ts
+++ app/libs/sanitize.ts
@@ -1,17 +1,27 @@
-import { isValid } from "date-fns";
+import { isValid, format } from "date-fns";
 
-export function sanitize(obj: { [key: string]: any}) {
-    const newObj: { [key: string]: any } = {};
-    for (const key in obj) {
-        if (typeof obj[key] === "string") {
-            newObj[key] = obj[key].trim();
-        } else if (typeof obj[key] === "object") {
-            if (isValid(obj[key])) {
-                newObj[key] = obj[key].toISOString();
-            }
-        } else {
-            newObj[key] = obj[key];
+export function sanitize(obj: { [key: string]: any }) {
+	const newObj: { [key: string]: any } = {};
+	for (const key in obj) {
+        switch (typeof obj[key]) {
+            case "string":
+                newObj[key] = obj[key].trim();
+                break;
+            case "object":
+                if (isValid(obj[key])) {
+                    newObj[key] = format(obj[key] as Date, "MM/dd/yyyy");
+                }
+                break;
+            case "boolean":
+                newObj[key] = obj[key] ? "true" : "false";
+                break;
+            case "bigint":
+                newObj[key] = obj[key].toNumber();
+                break;
+            default:
+                newObj[key] = obj[key];
+                break;
         }
-    }
-    return newObj;
+	}
+	return newObj;
 }
