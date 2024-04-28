729d828e1b794761ddac4eabfbc9a10c660d95d1
Avery Burke <DrFroid01@gmail.com>
2024-04-28 14:43:58 -0700

    Adds chunked transitions for large ring set updates

--- app/libs/visualization/createPizza.ts
+++ app/libs/visualization/createPizza.ts
@@ -106,26 +106,32 @@ function createPizza() {
 			};
 
 			updateRingColumn = function () {
-				// Every ring is gets its own animated transition.
-				// This can be very slow for large ring sets.
+				// Every ring should get its own animated transition.
+				// However, this can be very slow for large ring sets.
 				// In theory, changing the duration for each ring transition to a precentage of half a resonable duration
 				// should make the total transition time reasonable.
-				// However, I'm not seeing that behavior for very large ring sets.
-				// This could be due to some minimum transition time, but I can't find any documentation on that.
+				// In practice, this is not the case when the ring set is over about 30 rings.
+				// This discrepancy could be cause by a large number of d3 transition updates happening in rapid succession.
+				// For now the solution is to chunk the ring transitions into a few group transitions, when the ring set is large.
+				let chunkSize = Math.max(Math.round(ringSet.length / 10), 1);
 				bacgroundWorker.changeTransitionDuration(Math.round(200 / ringSet.length));
 				ringValue = (d: any) => d[ringColumn];
-				const ringExistTransitions = ringSet.reduce<{ [ring: string]: { innerRadius: number; outerRadius: number } }[]>((acc, ring, i) => {
-					const remainingRings = ringSet.slice(i);
-					const ringExit = Object.fromEntries(
-						remainingRings.map((ring, j) => {
-							const { outerRadius, innerRadius } = ringHeights[ring]!;
-							if (j === 0) return [ring, { innerRadius: outerRadius, outerRadius }];
-							return [ring, { innerRadius, outerRadius }];
-						})
-					);
-					acc.push(ringExit);
-					return acc;
-				}, []);
+				const ringExistTransitions: { [ring: string]: { innerRadius: number; outerRadius: number } }[] = [];
+				for (let i = 0; i < ringSet.length; i += chunkSize) {
+					const ringExist: { [ring: string]: { innerRadius: number; outerRadius: number } } = {};
+					for (let j = i; j < ringSet.length; j++) {
+						const ring = ringSet[j];
+						const { outerRadius, innerRadius } = ringHeights[ring]!;
+						// Transition the current ring to its outer radius, on this step.
+						// Remove the ring on the next step.
+						if (j === i || j < i + chunkSize) {
+							ringExist[ring] = { innerRadius: outerRadius, outerRadius };
+						} else {
+							ringExist[ring] = { innerRadius, outerRadius };
+						}
+					}
+					ringExistTransitions.push(ringExist);
+				}
 				ringExistTransitions.forEach((ringExit) => {
 					bacgroundWorker.updateRingHeights(ringExit);
 				});
@@ -140,21 +146,30 @@ function createPizza() {
 					sliceColors = Object.fromEntries(sliceSet.map((slice, i) => [slice, colorPallet[i % colorPallet.length]]));
 					bacgroundWorker.updateSliceColors(sliceColors);
 					bacgroundWorker.changeTransitionDuration(Math.round(200 / ringSet.length));
-					ringHeights = Object.fromEntries(ringSet.map((ring, i) => [ring, { innerRadius: 0, outerRadius: 0 }]));
+					ringHeights = Object.fromEntries(ringSet.map((ring) => [ring, { innerRadius: 0, outerRadius: 0 }]));
 					bacgroundWorker.updateRingHeights(ringHeights);
 					ringCount = Object.fromEntries(ringSet.map((ring) => [ring, data.filter((d) => ringValue(d) === ring).length]));
-					const ringHeightEnterTransition = ringSet.reduce<{ [ring: string]: { innerRadius: number; outerRadius: number } }[]>((acc, ring, i) => {
-						const remainingRings = ringSet.slice(i + 1);
-						const height = ringCount[ring]! * (pieRadius / data.length);
-						const innerRadius = i > 0 ? ringHeights[ringSet[i - 1]].outerRadius : 0;
-						const outerRadius = innerRadius + height;
-						ringHeights[ring] = { innerRadius, outerRadius };
-						remainingRings.forEach((ring) => {
-							ringHeights[ring] = { innerRadius: outerRadius, outerRadius };
-						});
-						acc.push({ ...ringHeights });
-						return acc;
-					}, []);
+					const ringHeightEnterTransition: { [ring: string]: { innerRadius: number; outerRadius: number } }[] = [];
+
+					// See the comments in updateRingColumn for an explanation of the chunking.
+					const chunkSize = Math.max(Math.round(ringSet.length / 10), 1);
+					for (let i = 0; i < ringSet.length; i += chunkSize) {
+						for (let j = i; j < i + chunkSize; j++) {
+							// on this step transition the current ring from a height of 0, at the outer most ring's outer radius, to its full height
+							const ring = ringSet[j];
+							const height = ringCount[ring]! * (pieRadius / data.length);
+							const innerRadius = j > 0 ? ringHeights[ringSet[j - 1]].outerRadius : 0;
+							const outerRadius = innerRadius + height;
+							ringHeights[ring] = { innerRadius, outerRadius };
+						}
+						for (let j = i + chunkSize; j < ringSet.length; j++) {
+							// the remaining rings should be at a height of 0, at the outer radius of the outer most ring in the chunk.
+							const ring = ringSet[j];
+							ringHeights[ring] = { innerRadius: ringHeights[ringSet[j - 1]].outerRadius, outerRadius: ringHeights[ringSet[j - 1]].outerRadius };
+						}
+						ringHeightEnterTransition.push({ ...ringHeights });
+					}
+
 					ringHeightEnterTransition.forEach((ringEnter) => {
 						bacgroundWorker.updateRingHeights(ringEnter);
 					});
@@ -182,7 +197,7 @@ function createPizza() {
 			updateSliceColumn = function () {
 				sliceValue = (d: any) => d[sliceColumn];
 				sliceCount = {};
-				// d3 easeQuadInOut causes jank when there are a lot of slices.
+				// d3 ease functions causes jank when there are a lot of slices.
 				bacgroundWorker.changeEase(sliceSet.length < 50 ? "easeQuadOut" : "easeIdentitiy");
 				bacgroundWorker.changeTransitionDuration(300);
 				sliceSet.forEach((slice) => {
@@ -196,7 +211,7 @@ function createPizza() {
 
 			updateSliceSet = function () {
 				if (resetSlices) {
-					// d3 easeQuadInOut causes jank when there are a lot of slices.
+					// d3 ease functions causes jank when there are a lot of slices.
 					bacgroundWorker.changeEase(sliceSet.length < 50 ? "easeQuadIn" : "easeIdentitiy");
 					sliceSet.forEach((slice) => {
 						sliceAngles[slice] = { startAngle: 0, endAngle: 0 };
@@ -217,7 +232,7 @@ function createPizza() {
 					})
 				);
 				bacgroundWorker.updateSliceAngles(sliceAngles);
-				bacgroundWorker.changeEase("easeIdentitiy")
+				bacgroundWorker.changeEase("easeIdentitiy");
 				bacgroundWorker.dequeue();
 			};
 		});
